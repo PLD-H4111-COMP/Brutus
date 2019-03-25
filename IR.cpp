13dd2c7851db72f31f9d9070a16be0e353eba355
Clement Florant <clement.florant@insa-lyon.fr>
2019-03-25 10:44:10 +0100

    Base de l'IR

--- IR.cpp
+++ IR.cpp
@@ -1,6 +1,87 @@
 #include "IR.h"
 
-IRStore::add_cfg(CFG* cfg) {
+
+// ****************************************************************************
+
+
+IRInstr::IRInstr(BasicBlock* bb_, Operation op, VarType t, std::vector<std::string> params){
+}
+
+void IRInstr::gen_asm(std::ostream &o){
+}
+
+
+// ****************************************************************************
+
+
+BasicBlock::BasicBlock(CFG* cfg, std::string entry_label){
+}
+
+void BasicBlock::gen_asm(std::ostream &o){
+}
+
+void BasicBlock::add_IRInstr(IRInstr::Operation op, VarType t, std::vector<std::string> params){
+}
+
+
+// ****************************************************************************
+
+
+void CFG::gen_asm(std::ostream& o){
+}
+std::string CFG::IR_reg_to_asm(std::string reg){
+    return "";
+}
+
+void CFG::gen_asm_prologue(std::ostream& o){
+}
+
+void CFG::gen_asm_epilogue(std::ostream& o){
+}
+
+
+int CFG::get_var_index(std::string name){
+    return 0;
+}
+
+/*VarType CFG::get_var_type(std::string name){
+
+}*/
+
+CFG::CFG(DefFonction* funcAst) : ast(funcAst), nextFreeSymbolIndex(0), nextBBnumber(0) {
+    bbs.push_back(new BasicBlock(this, "input"));
+    bbs.push_back(new BasicBlock(this, "output"));
+}
+
+
+std::string CFG::new_BB_name() {
+    std::string newName = "block_" + std::to_string(nextBBnumber++);
+}
+
+
+void CFG::add_bb(BasicBlock* bb) {
+    bbs.insert(bbs.end()-1, bb);
+}
+
+void CFG::add_to_symbol_table(std::string name, VarType t) {
+    SymbolType[name] = t;
+    SymbolIndex[name] = nextFreeSymbolIndex;
+    nextFreeSymbolIndex -= 4; //offset sera variable en fonction du type -> nextFreeSymbolIndex += t.size;
+}
+
+std::string CFG::create_new_tempvar(VarType t) {
+    std::string name = "!temp" + std::to_string(nextFreeSymbolIndex);
+    SymbolType[name] = t;
+    SymbolIndex[name] = nextFreeSymbolIndex;
+    nextFreeSymbolIndex -= 4; //offset sera variable en fonction du type -> nextFreeSymbolIndex += t.size;
+}
+
+
+// ****************************************************************************
+
+
+
+void IRStore::add_cfg(CFG* cfg) {
 	cfgs.push_back(cfg);
 }
 
@@ -10,27 +91,3 @@ IRStore::~IRStore() {
     }
 }
 
-CFG::CFG(DefFonction* funcAst) : ast(funcAst), nextFreeSymbolIndex(0), nextBBnumber(0) {
-    bbs.push_back(new BasicBlock(this, "input"));
-    bbs.push_back(new BasicBlock(this, "output"));
-}
-
-string CFG::new_BB_name() {
-    string newName = "block_"+to_string(nextBBnumber++);
-}
-
-void CFG::add_bb(BasicBlock* bb) {
-    bbs.insert(bbs.end()-1, bb);
-}
-
-void CFG::add_to_symbol_table(string name, Type t) {
-    SymbolType.insert(pair<string,Type>(name,t));
-    SymbolIndex.insert(pair<string,Type>(name,nextFreeSymbolIndex));
-    nextFreeSymbolIndex -= 4; //offset sera variable en fonction du type -> nextFreeSymbolIndex += t.size;
-}
-
-string CFG::create_new_tempvar(Type t) {
-    SymbolType.insert(pair<string,Type>("!temp"+to_string(nextFreeSymbolIndex),t));
-    SymbolIndex.insert(pair<string,Type>(name,nextFreeSymbolIndex));
-    nextFreeSymbolIndex -= 4; //offset sera variable en fonction du type -> nextFreeSymbolIndex += t.size;
-}
--- IR.h
+++ IR.h
@@ -4,14 +4,34 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <map>
 #include <initializer_list>
 
-// Declarations from the parser -- replace with your own
-#include "type.h"
-#include "symbole.h"
+
+
 class BasicBlock;
 class CFG;
 class DefFonction;
+
+
+
+// ****************************************************************************
+
+enum TypeEnum {INT_64};
+
+class VarType{
+public:
+    VarType(){}
+    inline int size() { return VAR_TYPE_SIZE[type]; }
+    virtual ~VarType(){}
+private:
+    TypeEnum type;
+    static std::map<TypeEnum, int> VAR_TYPE_SIZE; /* number of bytes for each var */
+};
+
+std::map<TypeEnum, int> VarType::VAR_TYPE_SIZE = { {INT_64, 8} };
+
+// ****************************************************************************
 
 
 //! The class for one 3-address instruction
@@ -34,20 +54,21 @@ class IRInstr {
 
 
 	/**  constructor */
-	IRInstr(BasicBlock* bb_, Operation op, Type t, vector<string> params);
+	IRInstr(BasicBlock* bb_, Operation op, VarType t, std::vector<std::string> params);
 	
 	/** Actual code generation */
-	void gen_asm(ostream &o); /**< x86 assembly code generation for this IR instruction */
+	void gen_asm(std::ostream &o); /**< x86 assembly code generation for this IR instruction */
 	
  private:
 	BasicBlock* bb; /**< The BB this instruction belongs to, which provides a pointer to the CFG this instruction belong to */
 	Operation op;
-	Type t;
-	vector<string> params; /**< For 3-op instrs: d, x, y; for ldconst: d, c;  For call: label, d, params;  for wmem and rmem: choose yourself */
+	VarType t;
+	std::vector<std::string> params; /**< For 3-op instrs: d, x, y; for ldconst: d, c;  For call: label, d, params;  for wmem and rmem: choose yourself */
 	// if you subclass IRInstr, each IRInstr subclass has its parameters and the previous (very important) comment becomes useless: it would be a better design. 
 };
 
 
+// ****************************************************************************
 
 
 
@@ -68,23 +89,24 @@ class IRInstr {
 
 class BasicBlock {
  public:
-	BasicBlock(CFG* cfg, string entry_label);
-	void gen_asm(ostream &o); /**< x86 assembly code generation for this basic block (very simple) */
+	BasicBlock(CFG* cfg, std::string entry_label);
+	void gen_asm(std::ostream &o); /**< x86 assembly code generation for this basic block (very simple) */
 
-	void add_IRInstr(IRInstr::Operation op, Type t, vector<string> params);
+	void add_IRInstr(IRInstr::Operation op, VarType t, std::vector<std::string> params);
 
 	// No encapsulation whatsoever here. Feel free to do better.
 	BasicBlock* exit_true;  /**< pointer to the next basic block, true branch. If nullptr, return from procedure */ 
 	BasicBlock* exit_false; /**< pointer to the next basic block, false branch. If null_ptr, the basic block ends with an unconditional jump */
-	string label; /**< label of the BB, also will be the label in the generated code */
+	std::string label; /**< label of the BB, also will be the label in the generated code */
 	CFG* cfg; /** < the CFG where this block belongs */
-	vector<IRInstr*> instrs; /** < the instructions themselves. */
+	std::vector<IRInstr*> instrs; /** < the instructions themselves. */
  protected:
 
  
 };
 
 
+// ****************************************************************************
 
 
 /** The class for the control flow graph, also includes the symbol table */
@@ -100,42 +122,54 @@ class CFG {
  public:
 	CFG(DefFonction* funcAst);
 
-	DefFonction* ast; /**< The AST this CFG comes from */
+
 	
-	void add_bb(BasicBlock* bb); 
+	void add_bb(BasicBlock* bb);
 
 	// x86 code generation: could be encapsulated in a processor class in a retargetable compiler
-	void gen_asm(ostream& o);
-	string IR_reg_to_asm(string reg); /**< helper method: inputs a IR reg or input variable, returns e.g. "-24(%rbp)" for the proper value of 24 */
-	void gen_asm_prologue(ostream& o);
-	void gen_asm_epilogue(ostream& o);
+	void gen_asm(std::ostream& o);
+	std::string IR_reg_to_asm(std::string reg); /**< helper method: inputs a IR reg or input variable, returns e.g. "-24(%rbp)" for the proper value of 24 */
+	void gen_asm_prologue(std::ostream& o);
+	void gen_asm_epilogue(std::ostream& o);
 
 	// symbol table methods
-	void add_to_symbol_table(string name, Type t);
-	string create_new_tempvar(Type t);
-	int get_var_index(string name);
-	Type get_var_type(string name);
+	void add_to_symbol_table(std::string name, VarType t);
+	std::string create_new_tempvar(VarType t);
+	int get_var_index(std::string name);
+//	VarType get_var_type(std::string name);
+
+
+	DefFonction* ast; /**< The AST this CFG comes from */
 
 	// basic block management
-	string new_BB_name();
+	std::string new_BB_name();
 	BasicBlock* current_bb;
 
  protected:
-	map <string, Type> SymbolType; /**< part of the symbol table  */
-	map <string, int> SymbolIndex; /**< part of the symbol table  */
+	std::map <std::string, VarType> SymbolType; /**< part of the symbol table  */
+	std::map <std::string, int> SymbolIndex; /**< part of the symbol table  */
 	int nextFreeSymbolIndex; /**< to allocate new symbols in the symbol table */
 	int nextBBnumber; /**< just for naming */
 	
-	vector <BasicBlock*> bbs; /**< all the basic blocks of this CFG*/
+	std::vector <BasicBlock*> bbs; /**< all the basic blocks of this CFG*/
 };
 
+
+
+
+// ****************************************************************************
+
+
+/*
+
+ */
 class IRStore {
 public :
 	IRStore();
     virtual ~IRStore();
 	void add_cfg(CFG* cfg);
 private :
-	vector<CFG*> cfgs;
+	std::vector<CFG*> cfgs;
 };
 
 
