40e6b91777ec21f587a2d4ad07f51563719dcf21
Paul Du <du.paul136@gmail.com>
2019-04-05 10:54:34 +0200

    Fix help style

--- main.cpp
+++ main.cpp
@@ -27,14 +27,10 @@ int main(int argc, char **argv)
     {
         cout << argv[0] << " [options] <input_file>" << endl
         << "[options] : -o <output_file> | -O | -a | --help" << endl << endl
-        << "-o <output_file> :" << endl
-        << "définit le nom du fichier de sortie" << endl
-        << "-O :" << endl
-        << "optimise la représentation en mémoire du programme" << endl
-        << "-a :" << endl
-        << "s'arrête avant la génération du fichier assembleur" << endl
-        << "--help :" << endl
-        << "affiche l'utilisation du programme" << endl << endl
+        << "-o <output_file> : définit le nom du fichier de sortie" << endl
+        << "-O : optimise la représentation en mémoire du programme" << endl
+        << "-a : s'arrête avant la génération du fichier assembleur" << endl
+        << "--help : affiche l'utilisation du programme" << endl << endl
         << "Comportement par défaut :" << endl
         << argv[0] << " <input_file>"  << endl
         << "analyse le fichier d'entrée, renvoie toutes les erreurs trouvées sur la sortie" << endl
